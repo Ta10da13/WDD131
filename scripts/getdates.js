e856c09a0f7704d6860d550e16df7824b943f0c8
Ta10da13 <geraldnyamangwe13@gmail.com>
2024-04-26 17:41:06 +0200

    PERSONAL SITE

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get the current year and last modified date and updates the footer
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+
+document.getElementById("currentyear").innerText = currentYear;
+document.getElementById('lastModified').innerText = document.lastModified;

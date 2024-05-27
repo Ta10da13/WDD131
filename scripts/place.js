1d4df9c7b0ab98fd99f74b871145b2ba12f5a908
Ta10da13 <geraldnyamangwe13@gmail.com>
2024-05-27 17:47:12 +0200

    Country

--- scripts/place.js
+++ scripts/place.js
@@ -1,7 +1,7 @@
 const displayList = (data, block) => {
     for (var i = 0; i<data.length; i++) {
         let li = document.createElement('li');
-        li.innerHTML = `<strong>${data[i][0]}:</strong> <span>${data[i][1]}</span>`;    
+        li.innerHTML = `<strong>${data[i][0]}:</strong><span>${data[i][1]}</span>`;    
         if(li) {
             block.appendChild(li);
         }
